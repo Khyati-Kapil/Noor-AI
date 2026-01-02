b20ce90eb50376366afb0c6e5443f327ccff0975
Khyati-Kapil <khyatekapil@gmail.com>
2026-01-02 22:33:11 +0530

    add security headers and update server configuration for CORS

--- client/noor-ai/vite.config.js
+++ client/noor-ai/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+    headers: {
+      'X-Frame-Options': 'ALLOWALL',
+      'Content-Security-Policy': "frame-ancestors *",
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 })
