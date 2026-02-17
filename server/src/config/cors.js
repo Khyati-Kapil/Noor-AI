f914721965784581da81357e0bf28c3a689ecc5c
Khyati-Kapil <khyatekapil@gmail.com>
2026-02-17 15:26:24 +0530

    feat: simplify CORS configuration to allow all origins

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,23 +1,7 @@
 import cors from "cors";
 
 const corsOptions = {
-  origin: function (origin, callback) {
-
-    
-    if (!origin) return callback(null, true);
-
-    const allowed = 
-      origin.includes("localhost") ||
-      origin.includes("127.0.0.1") ||
-      origin.endsWith("vercel.app");
-
-    if (allowed) {
-      callback(null, true);
-    } else {
-      console.log("Blocked by CORS:", origin);
-      callback(null, false);  
-    }
-  },
+  origin: true,        
   credentials: true
 };
 
